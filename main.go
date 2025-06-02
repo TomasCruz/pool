350535cb8af7ebb13ca803c28f42486e8b842214
Toma Krstic <toma.krstic.76@gmail.com>
2025-06-02 15:08:43 +0200

    better

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 
 var (
 	ErrBadParam       = errors.New("bad params")
-	ErrBadTask        = errors.New("bad task")
 	ErrPoolNotStarted = errors.New("pool not started")
 )
 
@@ -23,31 +22,26 @@ type Task struct {
 
 type WorkerPool struct {
 	noWorkers   int
-	maxTasks    int
 	started     bool
 	start       sync.Once
 	end         sync.Once
-	workersDone chan struct{}
+	workerGroup *sync.WaitGroup
 	tasks       chan Task
 	results     chan Task
 }
 
-func NewWorkerPool(noWorkers, maxTasks int) (*WorkerPool, error) {
-	if noWorkers <= 0 || noWorkers > 10 {
+func NewWorkerPool(noWorkers int) (*WorkerPool, error) {
+	if noWorkers <= 0 {
 		return nil, ErrBadParam
 	}
 
-	if maxTasks <= 0 || maxTasks > 100 {
-		return nil, ErrBadParam
-	}
-
+	maxTasks := 1024
 	return &WorkerPool{
 		noWorkers:   noWorkers,
-		maxTasks:    maxTasks,
 		started:     false,
 		start:       sync.Once{},
 		end:         sync.Once{},
-		workersDone: make(chan struct{}),
+		workerGroup: &sync.WaitGroup{},
 		tasks:       make(chan Task, maxTasks),
 		results:     make(chan Task, maxTasks),
 	}, nil
@@ -58,24 +52,24 @@ func (wp *WorkerPool) Results() (<-chan Task, error) {
 		return nil, ErrPoolNotStarted
 	}
 
-	wp.end.Do(func() {
-		close(wp.tasks)
-		for i := 0; i < wp.noWorkers; i++ {
-			<-wp.workersDone
-		}
-		close(wp.results)
-	})
-
 	return wp.results, nil
 }
 
-func (wp *WorkerPool) AddTask(task Task) error {
-	if task.F == nil {
-		return ErrBadTask
-	}
+func (wp *WorkerPool) Stop() {
+	wp.end.Do(func() {
+		close(wp.results)
+	})
+}
 
-	wp.tasks <- task
-	return nil
+func (wp *WorkerPool) AddTasks(tasks []Task) {
+	for _, t := range tasks {
+		if t.F == nil {
+			continue
+		}
+
+		wp.tasks <- t
+	}
+	close(wp.tasks)
 }
 
 func (wp *WorkerPool) Run(ctx context.Context) {
@@ -86,22 +80,27 @@ func (wp *WorkerPool) Run(ctx context.Context) {
 }
 
 func (wp *WorkerPool) worker(ctx context.Context) {
+	defer wp.workerGroup.Done()
+
 	for t := range wp.tasks {
 		select {
 		case <-ctx.Done():
-			wp.workersDone <- struct{}{}
 			return
 		default:
 			t.Res, t.Err = t.F(t.Arg)
 			wp.results <- t
 		}
 	}
-
-	wp.workersDone <- struct{}{}
 }
 
 func (wp *WorkerPool) startWorkers(ctx context.Context) {
 	for i := 0; i < wp.noWorkers; i++ {
+		wp.workerGroup.Add(1)
 		go wp.worker(ctx)
 	}
+
+	go func() {
+		wp.workerGroup.Wait()
+		wp.Stop()
+	}()
 }
--- main_test.go
+++ main_test.go
@@ -5,32 +5,34 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestWorkerPool_NewPool(t *testing.T) {
-	if _, err := NewWorkerPool(0, 2); err != ErrBadParam {
+	if _, err := NewWorkerPool(0); err != ErrBadParam {
 		t.Fatalf("expected error when creating pool with 0 workers, got: %v", err)
 	}
 
-	if _, err := NewWorkerPool(11, 2); err != ErrBadParam {
-		t.Fatalf("expected error when creating pool with 11 workers, got: %v", err)
-	}
-
-	if _, err := NewWorkerPool(-1, 2); err != ErrBadParam {
+	if _, err := NewWorkerPool(-1); err != ErrBadParam {
 		t.Fatalf("expected error when creating pool with -1 channel size, got: %v", err)
 	}
 }
 
 func TestWorkerPool_MultipleStartStopDontPanic(t *testing.T) {
-	wp, err := NewWorkerPool(5, 5)
+	wp, err := NewWorkerPool(5)
 	if err != nil {
 		t.Fatalf("error creating pool: %v", err)
 	}
 
 	// We're just checking to make sure multiple calls to start or stop
 	// don't cause a panic
-	wp.Run(context.TODO())
-	wp.Run(context.TODO())
+	ctx, cancel := context.WithTimeout(context.TODO(), 100*time.Millisecond)
+	defer cancel()
+
+	wp.Run(ctx)
+	wp.Run(ctx)
+	wp.Stop()
+	wp.Stop()
 }
 
 func sillyFunc(xAny any) (any, error) {
@@ -50,34 +52,27 @@ func sillyFunc(xAny any) (any, error) {
 func TestWorkerPool_Work(t *testing.T) {
 	var tasks []Task
 
-	taskNumber := 12
-	startFrom := int64(800000000)
+	taskNumber := 200
+	startFrom := int64(40000000)
 
-	for i := 0; i < taskNumber; i++ {
+	for i := range taskNumber {
 		tasks = append(tasks, Task{F: sillyFunc, Arg: startFrom + int64(i)})
 	}
 
-	wp, err := NewWorkerPool(4, taskNumber)
+	wp, err := NewWorkerPool(6)
 	if err != nil {
 		t.Fatalf("error making worker pool: %v", err)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithTimeout(context.TODO(), 1000*time.Millisecond)
 	defer cancel()
 
 	wp.Run(ctx)
-
-	for _, t := range tasks {
-		wp.AddTask(t)
-	}
+	wp.AddTasks(tasks)
 
 	resChannel, err := wp.Results()
 	if err != nil {
 		t.Fatalf("error fetching worker pool results: %v", err)
-	}
-
-	if len(resChannel) != taskNumber {
-		t.Fatalf("resChannel length wrong: %v", len(resChannel))
 	}
 
 	for t := range resChannel {
